5bda6e115013e45024a69d79d5ec8dfc6d4fa3e7
Sebastian Slamanig <s.slamanig@agentur-loop.com>
2024-07-30 20:02:28 +0200

    Add hyprland copr and utils

--- build.sh
+++ build.sh
@@ -12,9 +12,36 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Add COPR repositories (credits go to https://github.com/cjuniorfox/hyprland-atomic/)
+for i in solopasha/hyprland; do
+    MAINTAINER="${i%%/*}"
+    REPOSITORY="${i##*/}"
+    curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
+done
+
 # this installs a package from fedora repos
-rpm-ostree install screen
-rpm-ostree install fastfetch
+rpm-ostree install hyprland \
+    xdg-desktop-portal-hyprland \
+    hyprpaper \
+    hypridle \
+    hyprlock \
+    hyprnome \
+    hyprpicker \
+    hyprshot \
+    waybar-git \
+    dunst \
+    wlogout \
+    alacritty \
+    wofi \
+    network-manager-applet \
+    bluez \
+    blueman \
+    cliphist \
+    brightnessctl \
+    wpctl \
+    playerctl \
+    fira-code-fonts
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
